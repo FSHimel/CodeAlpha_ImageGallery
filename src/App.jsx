5c054893b821c3a217fef0c06a2d62bf1ec7e669
FSHimel <shahriarfahim067@gmail.com>
2026-08-19 14:03:30 +0600

    all the cards shown according to filter and animation transition added

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,34 @@ import { IoIosArrowDropdown } from "react-icons/io";
 import "./App.css";
 import Container from "./Component/Container";
 import Header from "./Component/Header/Header";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
+import images from "./../public/data.js";
+import ImageCard from "./Component/ImageCard/ImageCard.jsx";
 
 const categories = ["All", "Nature", "Animals", "Travel", "Architecture"];
 
 function App() {
-  const [clicked, setClicked] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredImages =
+    selectedCategory === "All"
+      ? images
+      : images.filter((image) => image.category === selectedCategory);
 
   return (
     <div className="pt-5">
       <Container>
         <Header></Header>
         <div className="hidden md:flex gap-4 md:gap-6 bg-[#C9CFCF] w-fit px-8 py-2 rounded-full text-[#111313]">
-          {categories.map((category, index) => (
+          {categories.map((category) => (
             <div key={category}>
               <motion.button
-                onClick={() => setClicked(index)}
                 onClick={() => setSelectedCategory(category)}
                 whileHover={{ y: -2, x: 2 }}
                 whileTap={{ scale: 0.95 }}
                 className={`px-3 py-2 cursor-pointer ${
-                  clicked === index ? "font-bold text-black" : ""
+                  selectedCategory === category ? "font-bold text-black" : ""
                 }`}
               >
                 {category}
@@ -33,7 +38,7 @@ function App() {
           ))}
         </div>
 
-        {/* only sho when it is smaller device */}
+        {/* only show when it is smaller device */}
         <div className="dropdown md:hidden">
           <div
             tabIndex={0}
@@ -46,14 +51,13 @@ function App() {
             tabIndex="-1"
             className="dropdown-content menu bg-[#C9CFCF] text-[#111313] rounded-box z-1 w-52 p-2 shadow-sm"
           >
-            {categories.map((category, index) => (
+            {categories.map((category) => (
               <div key={category}>
                 <motion.button
-                  onClick={() => setClicked(index)}
                   onClick={() => setSelectedCategory(category)}
                   whileTap={{ scale: 0.95 }}
                   className={`px-1 py-3 ${
-                    clicked === index ? "font-bold text-black" : ""
+                    selectedCategory === category ? "font-bold text-black" : ""
                   }`}
                 >
                   {category}
@@ -61,6 +65,16 @@ function App() {
               </div>
             ))}
           </ul>
+        </div>
+
+        {/* Cards where the images will be shown */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-5">
+          {filteredImages.map((image) => (
+            <AnimatePresence mode="popLayout">
+              <ImageCard key={image.id} image={image}></ImageCard>
+            </AnimatePresence>
+          ))}
         </div>
       </Container>
     </div>
---
+++ src/Component/ImageCard/ImageCard.jsx
@@ -0,0 +1,41 @@
+import { motion } from "motion/react";
+
+function ImageCard({ image }) {
+  return (
+    <motion.div
+      key={image.id}
+      layout
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      exit={{ opacity: 0, scale: 0.9, y: -20 }}
+      transition={{
+        duration: 0.7,
+        ease: "easeOut",
+      }}
+      whileHover={{
+        scale: 1.05,
+        transition: {
+          duration: 0.1,
+          ease: "easeOut",
+        },
+      }}
+      className="shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] rounded-xl cursor-pointer"
+    >
+      <img
+        src={image.image}
+        alt={image.title}
+        className="w-full h-80 rounded-xl"
+      />
+      <div className="my-3 flex justify-between items-center px-3">
+        <p className="text-black font-bold">{image.title}</p>
+        <p className="text-black">
+          <span className="font-bold">Category: </span>
+          {image.category}
+        </p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default ImageCard;
